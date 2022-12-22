6ccc1bdcd2056d505566b0e6b44ca20b5cbad2f6
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2022-12-22 11:00:03 +0200

    .

---
+++ src/pages/HomeStyled.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { colors } from "../utils/colors";
+import { device } from "../utils/mixins";
+
+export const BlockLink = styled.div`
+  display: flex;
+  margin-top: 60px;
+  ${device.desktop} {
+    margin-left: 75px;
+  }
+`;
+
+export const ButtonLink = styled(NavLink)`
+  ${device.tablet} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    width: 138px;
+    height: 40px;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0.02em;
+    color: ${colors.black};
+    background-color: #fafbfd;
+    border-radius: 16px 16px 0 0;
+  }
+
+  &.active {
+    color: ${colors.orange};
+    background-color: #fefefe;
+  }
+`;

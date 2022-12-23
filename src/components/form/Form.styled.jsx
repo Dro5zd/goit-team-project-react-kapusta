625d475c1385bad15a4dcffced1bfeecea309f45
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-12-23 17:04:16 +0200

    fix style home page

---
+++ src/components/form/Form.styled.jsx
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+import { device } from "../../utils/mixins";
+import { colors } from "../../utils/colors";
+import { ReactComponent as Calendar } from "../../assets/images/svg/calendar.svg";
+
+export const FormGroup = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  ${device.desktop} {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const BlockForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${device.tablet} {
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+  }
+`;
+
+export const BlockPicker = styled.div`
+  display: none;
+  ${device.tablet} {
+    display: flex;
+    align-items: center;
+    /* display: block;
+    position: relative; */
+
+    /* img {
+      position: absolute;
+      top: 12px;
+      left: -14px;
+    } */
+  }
+`;
+
+export const CalendarIcon = styled(Calendar)`
+  width: 20px;
+  min-width: 20px;
+  height: 20px;
+`;
+
+export const InputProduct = styled.input`
+  width: 280px;
+  height: 44px;
+  border: 2px solid #ffffff;
+  border-radius: 16px 0 0 0;
+  padding: 2px 20px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  background-color: #f5f6fb;
+  outline: none;
+  color: #c7ccdc;
+  &::placeholder {
+    color: #c7ccdc;
+  }
+  ${device.tablet} {
+    /* margin-left: 44px; */
+    width: 186px;
+    background-color: #ffffff;
+    border-color: #f5f6fb;
+  }
+  ${device.desktop} {
+    margin-left: 32px;
+    width: 290px;
+  }
+`;
+
+export const BlockButton = styled.div`
+  margin-top: 80px;
+  display: flex;
+  gap: 20px;
+  ${device.tablet} {
+    margin-top: 32px;
+    gap: 15px;
+  }
+
+  ${device.desktop} {
+    margin-top: 0;
+    margin-left: 32px;
+    gap: 16px;
+  }
+`;
+
+export const ButtonInput = styled.button`
+  padding: 16px 35px;
+  background: ${colors.orange};
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 14px;
+  color: ${colors.white};
+
+  ${device.tablet} {
+    min-width: 125px;
+  }
+
+  ${device.desktop} {
+    min-width: 136px;
+  }
+`;
+
+export const ButtonClear = styled.button`
+  padding: 16px 30px;
+  background: #f5f6fb;
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 14px;
+  color: #52555f;
+  box-shadow: 1px 2px 5px rgba(170, 178, 197, 0.4);
+  ${device.tablet} {
+    min-width: 125px;
+  }
+
+  ${device.desktop} {
+    min-width: 136px;
+  }
+`;
+
+//  @media screen and (max-width: 479px) {
+//     background-size: 100% 258px;
+//     background-repeat: no-repeat;
+//     background-image: url(${fon});
+//   }

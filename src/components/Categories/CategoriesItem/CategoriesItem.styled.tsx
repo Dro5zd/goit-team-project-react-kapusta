b641d159a1fc05b936abfe6cfcfee894d085c069
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-12-27 08:19:08 +0200

    fix

--- src/components/Categories/CategoriesItem/CategoriesItem.styled.tsx
+++ src/components/Categories/CategoriesItem/CategoriesItem.styled.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { colors } from "../../../utils/colors";
 import { device } from "../../../utils/mixins";
-import {ReactComponent as products} from "../../../assets/images/svg/expenses/products.svg";
+import { ReactComponent as products } from "../../../assets/images/svg/expenses/products.svg";
 
 interface IIcon {
-    active: string
+  active: string;
 }
 export const CategoriesItemWrapper = styled.div`
   display: flex;
@@ -13,10 +13,8 @@ export const CategoriesItemWrapper = styled.div`
   justify-content: center;
   width: 84px;
   ${device.tablet} {
-
   }
   ${device.desktop} {
-
   }
 `;
 
@@ -30,59 +28,52 @@ export const CategoriesTotalSum = styled.p`
   justify-content: center;
   letter-spacing: 0.04em;
   text-transform: uppercase;
-  color: #52555F;
+  color: #52555f;
 
   ${device.tablet} {
-
   }
 
   ${device.desktop} {
-
   }
-`;export const CategoriesIconWrapper = styled.div`
+`;
+export const CategoriesIconWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  
+
   svg {
     width: 56px;
     height: 56px;
-    rect{
+    rect {
       fill: ${colors.lightOrange};
     }
   }
 
   ${device.tablet} {
-
   }
 
   ${device.desktop} {
-
   }
 `;
 
-export const CategoriesIcon = styled(products)`
-  width: 56px;
-  height: 56px;
-  fill: ${(p: IIcon) => (p.active === 'true' ? `${colors.orange}` : `#071F41`)};
-  ${device.tablet} {
+// export const CategoriesIcon = styled(products)`
+//   width: 56px;
+//   height: 56px;
+//   fill: ${(p: IIcon) => (p.active === "true" ? `${colors.orange}` : `#071F41`)};
+//   ${device.tablet} {
+//   }
+//   ${device.desktop} {
+//   }
+//   @media screen and (min-width: 768px) {
+//     width: 306.52px;
+//     height: 77.69px;
+//   }
 
-  }
-  ${device.desktop} {
-
-  }
-  @media screen and (min-width: 768px) {
-    width: 306.52px;
-    height: 77.69px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    width: 377px;
-    height: 120px;
-  }
-`;
-
-
+//   @media screen and (min-width: 1280px) {
+//     width: 377px;
+//     height: 120px;
+//   }
+// `;
 
 export const CategoriesName = styled.p`
   width: 84px;
@@ -97,11 +88,9 @@ export const CategoriesName = styled.p`
   letter-spacing: 0.02em;
   text-transform: uppercase;
 
-  color: #52555F;
+  color: #52555f;
   ${device.tablet} {
- 
   }
   ${device.desktop} {
-
   }
 `;
--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -7,18 +7,16 @@ import CategoriesList from "../../components/Categories/CategoriesList/Categorie
 import SelDataPicker from "../../components/DatePicker/DatePicker";
 import { MainChart } from "../../components/Chart/MainChart";
 import { getReportsByPeriod } from "../../http/services.user";
-import { ExpensesTypes } from "../../components/ExpensesTypes/ExpensesTypes";
 import { useFetch } from "../../hooks/useFetch";
 import { Loader } from "../../components/Loader/Loader";
 import { BackgroundMobil } from "../../components/BackgroundMobil/BackgroundMobil";
 import { SubHeaderReport } from "../../components/SubHeaderReport/SubHeaderReport";
-import StyledBarChart from '../../components/Chart/StyledBarChart';
-import TestChart from '../../components/Chart/StyledBarChart';
 
 const Report = () => {
   const categories = getReportsByPeriod();
   const [isLoading, setIsLoading] = useState(false);
   const [categoriesArr, setCategoriesArr] = useState([]);
+  const [uniqueArr, setUniqueArr] = useState([]);
   const [chartData, setChartData] = useState({});
   const [categoriesIncomes, setCategoriesIncomes] = useState({});
 
@@ -28,21 +26,48 @@ const Report = () => {
       delete data.data.total;
       setChartData(data);
     }
+    const arr = new Set(categoriesArr);
+    setUniqueArr(arr);
   }, [categoriesArr]);
+
+  console.log("UniqueA", categoriesArr);
+
+  // const getData = useCallback((params) => {
+  //   getReportsByPeriod(params).then((data) => {
+  //     for (const dataKey in data.expenses) {
+  //       if (typeof data.expenses[dataKey] === "object") {
+  //         for (const key in data.expenses[dataKey]) {
+  //           setCategoriesArr((prevState) => [
+  //             ...prevState,
+  //             {
+  //               category: key,
+  //               total: data.expenses[dataKey][key].total,
+  //               data: data.expenses[dataKey][key],
+  //             },
+  //           ]);
+  //         }
+  //       }
+  //     }
+  //     setIsLoading(false);
+  //   });
+  // }, []);
 
   const getData = useCallback((params) => {
     getReportsByPeriod(params).then((data) => {
-      for (const dataKey in data.expenses) {
-        if (typeof data.expenses[dataKey] === "object") {
-          for (const key in data.expenses[dataKey]) {
-            setCategoriesArr((prevState) => [
-              ...prevState,
-              {
-                category: key,
-                total: data.expenses[dataKey][key].total,
-                data: data.expenses[dataKey][key],
-              },
-            ]);
+      console.log("data.expenses", data.expenses.expensesData, data.expenses);
+      if (data?.expenses?.expensesData) {
+        const arr = [];
+        for (const key in data?.expenses?.expensesData) {
+          const isContainCategory = categoriesArr.some((el) => {
+            return el.category === key;
+          });
+          if (!isContainCategory) {
+            arr.push({
+              category: key,
+              total: data?.expenses?.expensesData[key].total,
+              data: data?.expenses?.expensesData[key],
+            });
+            setCategoriesArr(arr);
           }
         }
       }
@@ -63,7 +88,7 @@ const Report = () => {
   }
 
   const onclickHandle = (e, title) => {
-    const data = categoriesArr.find((el) => el.category === title);
+    const data = categoriesArr?.find((el) => el.category === title);
     delete data.data.total;
     setChartData(data);
   };
@@ -80,10 +105,7 @@ const Report = () => {
             />
             {/* <ExpensesTypes /> */}
           </Box>
-          <Box page="report">
           {chartData?.data && <MainChart chartData={chartData?.data} />}
-          {/*{chartData?.data && <StyledBarChart chartData={chartData?.data}/>}*/}
-          </Box>
         </Container>
       </BackgroundMobil>
 
@@ -97,10 +119,7 @@ const Report = () => {
             />
             {/* <ExpensesTypes /> */}
           </Box>
-          <Box page="report">
           {chartData?.data && <MainChart chartData={chartData?.data} />}
-          {/*{chartData?.data && <StyledBarChart chartData={chartData?.data}/>}*/}
-        </Box>
         </Container>
       </BackgroundMain>
     </>
